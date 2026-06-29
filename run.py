46990a9ace94b06cc7efafcdc19919a89724bc19
HmaydaSalem <166590705+HmaydaSalem@users.noreply.github.com>
2026-06-29 13:33:11 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from app import create_app
 
+# Créer l'application et l'assigner à une variable nommée 'app'
 app = create_app()
 
+# Vercel n'a pas besoin de cette partie, mais vous pouvez la laisser pour le local
 if __name__ == "__main__":
-    # accessible en local sur le réseau (utile pour tester depuis un téléphone
-    # connecté au même wifi pendant le développement)
     app.run(host="0.0.0.0", port=5000, debug=True)
